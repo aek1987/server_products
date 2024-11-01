9d867759044fe29e9e0aadc42dcb4f67da3033f4
abdelkader nekkaa <nekaaabdelkader1987@gmail.com>
2024-11-01 12:13:28 +0100

    add produit detail

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -7,9 +7,6 @@ router.get('/clientstel/:phone', customersControler.gettCustomerByTel);
 router.get('/clients', customersControler.getAllCustumers);
 router.get('/clients/:id', customersControler.getcustomerById); 
 router.get('/commande_client/:id', customersControler.getorderCustemuer);//tous les comande client
-<<<<<<< HEAD
-router.get('/products_commande/:id', customersControler.getproductorder);// tous les produit d un commande
-=======
 
->>>>>>> 6cdf5dbee109ce7f7b9925681616ee28b1fdd508
+router.get('/products_commande/:id', customersControler.getproductorder);// tous les produit d un commande
 export default router; // Exportation par défaut du routeur
